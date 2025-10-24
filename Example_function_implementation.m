efdf6a236dcb3443cbbfc53977d1d9802e23541d
jfhafer <83718606+jfhafer@users.noreply.github.com>
2025-10-24 08:50:14 -0400

    Upload starter code for using functions

---
+++ Example_function_implementation.m
@@ -0,0 +1,47 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Example implementation code provided as a supplement to
+% JA Mihy, M Wagatsuma, SM Cain, JF Hafer, A functional sensor-to-segment 
+% calibration method reduces the effects of varied sensor placement on 
+% estimates of segment angular excursion, J Appl Biomech
+% 
+% See notes within functions and example_data.mat for details
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Load example data
+load('example_data.mat')
+
+%% Example func_S2S_orientation implementation
+align = 1;
+a = Cal.Pelvis1.a;
+w = Cal.Pelvis1.w;
+gravi = Cal.gravi;
+roti = Cal.rotiP;
+
+Pelvis1.R_bodyX = func_S2S_orientation(align,a,w,gravi,roti);
+
+align = 2;
+a = Cal.RShank1.a;
+w = Cal.RShank1.w;
+gravi = Cal.gravi;
+roti = Cal.rotiR;
+
+Shank1.R_bodyZ = func_S2S_orientation(align,a,w,gravi,roti);
+
+%% Example gait_ID_fft implementation
+%Note that the GaitTrials data includes only a single sensor per segment
+signal = GaitTrials.RShank.w;
+Fs = 128;
+windowsize = 5;
+
+[strt,nd,fig] = gait_ID_fft(signal,Fs,windowsize);
+
+%% Example gait_event_cwt implementation
+signal_a = BoutExample.RFoot.a_world;
+signal_w = BoutExample.RFoot.w_world;
+filter = 1;
+Fs = 128;
+LPfreq = 4;
+
+[TO,HS,ftd] = gait_event_cwt(signal_a,signal_w,filter,Fs,LPfreq);
